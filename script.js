42fb27c064d3def4b901de3a0760c896425baffe
Soham Desai <150273704+ItsMeSoham6508@users.noreply.github.com>
2024-10-10 13:21:14 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,15 +1,35 @@
 function showVid() {
+    const modal = document.getElementById("videoModal");
+    modal.style.display = "block";
 
+    setTimeout(() => {
+        modal.classList.add('show');
+    }, 10);
 }
 
 function hideVid() {
+    const modal = document.getElementById("videoModal");
+    modal.classList.remove('show');
 
+    setTimeout(() => {
+        modal.style.display = "none";
+    }, 10);
 }
 
 function showVidphone() {
+    const modal = document.getElementById("videoModal-side");
+    modal.style.display = "block";
 
+    setTimeout(() => {
+        modal.classList.add('show');
+    }, 10);
 }
 
 function hideVidphone() {
+    const modal = document.getElementById("videoModal-side");
+    modal.classList.remove('show');
 
+    setTimeout(() => {
+        modal.style.display = "none";
+    }, 10);
 }
