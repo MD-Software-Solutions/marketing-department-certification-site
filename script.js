a377045c1b55ada3c5087ed7ec74b2a0e34204c2
KhangNguyen872 <147101111+KhangNguyen872@users.noreply.github.com>
2024-10-10 22:01:21 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,35 +1,31 @@
 function showVid() {
-    const modal = document.getElementById("videoModal");
+    var modal = document.getElementById("videoModal");
     modal.style.display = "block";
-
-    setTimeout(() => {
-        modal.classList.add('show');
+    setTimeout(function() {
+        modal.classList.add("show");
     }, 10);
 }
 
 function hideVid() {
-    const modal = document.getElementById("videoModal");
-    modal.classList.remove('show');
-
-    setTimeout(() => {
+    var modal = document.getElementById("videoModal");
+    modal.classList.remove("show");
+    setTimeout(function() {
         modal.style.display = "none";
-    }, 10);
+    }, 500);
 }
 
 function showVidphone() {
-    const modal = document.getElementById("videoModal-side");
+    var modal = document.getElementById("videoModal-side");
     modal.style.display = "block";
-
-    setTimeout(() => {
-        modal.classList.add('show');
+    setTimeout(function() {
+        modal.classList.add("show");
     }, 10);
 }
 
 function hideVidphone() {
-    const modal = document.getElementById("videoModal-side");
-    modal.classList.remove('show');
-
-    setTimeout(() => {
+    var modal = document.getElementById("videoModal-side");
+    modal.classList.remove("show");
+    setTimeout(function() {
         modal.style.display = "none";
-    }, 10);
+    }, 500);
 }
